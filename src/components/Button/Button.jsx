de4bb1c770d504a36b64147b6c985a5ef63f52c4
Waldemar <vladislavv.maxs@gmail.com>
2020-10-22 23:19:58 +0300

    add categories

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./Button.module.scss";
+
+const Button = ({ title, className, clickCallBack, disable }) => {
+  return (
+    <div
+      className={`${disable ? style.wrapper : style.wrapperDisable} ${className}`}
+      onClick={disable ? clickCallBack : null}
+    >
+      <span>{title}</span>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  title: PropTypes.string,
+  className: PropTypes.string,
+  clickCallBack: PropTypes.func,
+  disable: PropTypes.bool
+};
+
+Button.defaultProps = {
+  title: "ok",
+  className: "",
+  clickCallBack: () => {},
+  disable: true
+};
+
+export default Button;
